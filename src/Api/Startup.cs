435990150200e3237e09774ae308d0353fd63ae9
George <gnthackray@btinternet.com>
2019-12-21 16:27:28 +0000

    pre deployment work commit

---
+++ src/Api/Startup.cs
@@ -0,0 +1,62 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Mvc.NewtonsoftJson;
+
+namespace Api
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddRazorPages();
+
+            services.AddMvcCore()//(o=>o.EnableEndpointRouting =false)
+                .AddAuthorization()
+                .AddNewtonsoftJson();
+
+         
+
+            services.AddAuthentication("Bearer")
+                .AddJwtBearer("Bearer", options =>
+                {
+                    options.Authority = "http://localhost:5000";
+                    options.RequireHttpsMetadata = false;
+
+                    options.Audience = "api1";
+                });
+
+            services.AddCors(options =>
+            {
+                // this defines a CORS policy called "default"
+                options.AddPolicy("default", policy =>
+                {
+                    policy.WithOrigins("http://localhost:5003")
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseCors("default");
+            app.UseAuthentication();
+            // app.UseMvc();
+            app.UseRouting();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
---
+++ src/IdentityServer/Startup.cs
@@ -0,0 +1,251 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer.Data;
+using IdentityServer4;
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace IdentityServer
+{
+    public class Startup
+    {
+        public IHostingEnvironment Environment { get; }
+        public IConfiguration Configuration { get; }
+
+        public Startup(IHostingEnvironment environment, IConfiguration configuration)
+        {
+            Environment = environment;
+            Configuration = configuration;
+        }
+
+        private void InitializeDatabase(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+              //  serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+
+                var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+                context.Database.Migrate();
+                if (!context.Clients.Any())
+                {
+                    foreach (var client in Config.GetClients())
+                    {
+                        context.Clients.Add(client.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.IdentityResources.Any())
+                {
+                    foreach (var resource in Config.GetIdentityResources())
+                    {
+                        context.IdentityResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.ApiResources.Any())
+                {
+                    foreach (var resource in Config.GetApis())
+                    {
+                        context.ApiResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+            }
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
+          
+            services.AddControllersWithViews();
+            services.AddRazorPages().AddSessionStateTempDataProvider();
+
+             
+            services.AddSession();
+
+
+            services.AddAuthorization();
+
+          //  services.AddDbContext<ApplicationDbContext>(builder =>
+          //   builder.UseSqlServer(connectionString, sqlOptions => sqlOptions.MigrationsAssembly(migrationsAssembly)));
+
+
+            var connectionString = Configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<ApplicationDbContext>(builder =>
+             builder.UseSqlServer(connectionString, sqlOptions => sqlOptions.MigrationsAssembly(migrationsAssembly)));
+
+ 
+
+           
+            //using (var context = new ApplicationDbContext(new DbContextOptions<ApplicationDbContext>()))
+            //{
+            //    context.Users.Add(new AppUser() { Id = DateTime.Now.Ticks.ToString(), FirstName = "bob", LastName = "smith" });
+
+            //    context.SaveChanges();
+            //}
+
+            var builder = services.AddIdentityServer()
+                .AddConfigurationStore(options =>
+                {
+                    options.ConfigureDbContext = b =>
+                        b.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"),
+                            sql => sql.MigrationsAssembly(migrationsAssembly));
+                })
+                .AddOperationalStore(options =>
+                {
+                    options.ConfigureDbContext = b =>
+                        b.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"),
+                            sql => sql.MigrationsAssembly(migrationsAssembly));
+
+                    // this enables automatic token cleanup. this is optional.
+                    options.EnableTokenCleanup = true;
+                });
+                //.AddAspNetIdentity<IdentityUser>();
+
+            if (Environment.IsDevelopment())
+            {
+                builder.AddDeveloperSigningCredential();
+            }
+            else
+            {
+                throw new Exception("need to configure key material");
+            }
+
+            services.AddAuthentication()
+                .AddGoogle("Google", options =>
+                {
+                   
+                    //options.Scope
+                    options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+                    options.ClaimActions.MapJsonKey("urn:google:picture", "picture", "url");
+                    options.ClaimActions.MapJsonKey("urn:google:locale", "locale", "string");
+                    options.ClaimActions.MapJsonKey("urn:google:email", "email", "string");
+
+                    options.ClientId = "183174195107-ud3u00alnb92q6vn4l54s7stpqbn3n4c.apps.googleusercontent.com";
+                    options.ClientSecret = "rm012zhRHzcP8axvgskTNfX5";
+
+                    options.Events.OnCreatingTicket = ctx =>
+                    {
+
+                        
+                        List<AuthenticationToken> tokens = ctx.Properties.GetTokens().ToList();
+
+                        tokens.Add(new AuthenticationToken()
+                        {
+                            Name = "TicketCreated",
+                            Value = DateTime.UtcNow.ToString()
+                        });
+
+                        ctx.Properties.StoreTokens(tokens);
+
+                        return Task.CompletedTask;
+                    };
+
+                    options.AuthorizationEndpoint += "?prompt=consent"; // Hack so we always get a refresh token, it only comes on the first authorization response
+                    options.AccessType = "offline";
+                    options.SaveTokens = true;
+                })
+                //.AddOpenIdConnect("oidc", "OpenID Connect", options =>
+                //{
+                //    options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+                //    options.SignOutScheme = IdentityServerConstants.SignoutScheme;
+                //    options.SaveTokens = true;
+                //    options.RequireHttpsMetadata = false;
+                //    options.Authority = "http://localhost:5000";
+                //    options.ClientId = "implicit";
+
+                //    //options.TokenValidationParameters = new TokenValidationParameters
+                //    //{
+                //    //    NameClaimType = "name",
+                //    //    RoleClaimType = "role"
+                //    //};
+                //})
+                .AddJwtBearer("Bearer", options =>
+                {
+                    options.Authority = "http://localhost:5000";
+                    options.RequireHttpsMetadata = false;
+
+                    options.Audience = "api1";
+                })
+                 ; 
+
+            services.AddCors(options =>
+            {
+                // this defines a CORS policy called "default"
+                options.AddPolicy("default", policy =>
+                {
+                   // policy.WithOrigins("http://localhost:1234")
+                   policy.AllowAnyOrigin()
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                });
+            });
+
+        //    services.AddIdentity<AppUser, IdentityRole>()
+           //         .AddEntityFrameworkStores<ApplicationDbContext>();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            // InitializeDatabase(app);
+            app.UseCors("default");
+             
+            app.UseAuthentication();
+
+
+
+            //app.Use(async (context, next) =>
+            //{
+            //    await next.Invoke();
+            //});
+
+
+            if (Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+
+            app.UseIdentityServer();
+            app.UseAuthorization();
+            
+        
+
+            app.UseCookiePolicy();
+            app.UseSession(); // This must come before "UseMvc()"
+                              //  app.UseHttpContextItemsMiddleware();
+
+ 
+
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
+            });
+            //  app.UseMvcWithDefaultRoute();
+        }
+    }
+}
